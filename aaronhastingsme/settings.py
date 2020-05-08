fe934ae746a0decf5ad42f4bec15caa2b009c48d
Aaron Hastings <aaron@aaronhastings.me>
2020-05-08 18:23:20 +0100

    * Changed Django DEBUG setting to an environment variable.

--- aaronhastingsme/settings.py
+++ aaronhastingsme/settings.py
@@ -25,7 +25,7 @@ SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG_ENABLED', default=False)
 
 ALLOWED_HOSTS = [
     'localhost', '127.0.0.1', 'www.aaronhastings.me', 'beta.aaronhastings.me'
